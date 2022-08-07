310713a5f70b025b210a258175240b56303e5df5
ashleygracejefferson <agj7854@stern.nyu.edu>
2022-08-07 00:34:44 -0400

    Zipcode field and label added

--- trip_planner.py
+++ trip_planner.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def trip_planner():
-    return "<h1>Trip Planner</h1>"
+    return """<h1>Trip Planner</h1>
+        <form>
+            <label for="Zipcode"> Please input the zipcode of where you wish to travel:</label><br>
+             <input type="text" id="Zipcode" name="Zipcode"><br>
+        </form>
+
+    """
